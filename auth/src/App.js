ba992024ae9d6d3086c7f7339f85682be64f4e02
Daniel <d.chen323@gmail.com>
2018-08-13 13:22:19 -0700

    Create logout functionality

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import { Header } from "./component/common";
+import { Header, Button, Spinner, Card } from "./component/common";
 import firebase from "firebase";
 import secret from "./config/secret";
 import LoginForm from "./component/LoginForm";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { loggedIn: null };
+  }
+
   componentDidMount() {
     const {
       apiKey,
@@ -21,12 +26,33 @@ export default class App extends Component {
       storageBucket,
       messagingSenderId
     });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>;
+    } else if (this.state.loggedIn === null) {
+      return <Spinner size="large" />;
+    } else {
+      return <LoginForm />;
+    }
+  }
+
   render() {
+    let loggedIn;
+
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
