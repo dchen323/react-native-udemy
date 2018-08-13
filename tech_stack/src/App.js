6d610f3cb8aa60769c5f2a8d16c16d60e926253a
Daniel <d.chen323@gmail.com>
2018-08-13 14:01:20 -0700

    Initial commit of new project

---
+++ tech_stack/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Header } from "./components/common";
+
+const App = () => {
+  return (
+    <Provider store={createStore(rootReducer)}>
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
