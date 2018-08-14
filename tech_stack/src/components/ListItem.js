bb649397956a541f2806ee9296c0e96b0a538ace
Daniel <d.chen323@gmail.com>
2018-08-14 13:04:34 -0700

    Create actions to get libraries

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { connect } from "react-redux";
+import { Text, TouchableWithoutFeedback, View } from "react-native";
 import { CardItem } from "./common";
+import * as actions from "../actions";
 
-export default class ListItem extends Component {
+class ListItem extends Component {
   render() {
-    const { title } = this.props.library.item;
+    const { title, id } = this.props.library.item;
     const { titleStyle } = styles;
     return (
-      <CardItem>
-        <Text style={titleStyle}>{title}</Text>
-      </CardItem>
+      <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+        <View>
+          <CardItem>
+            <Text style={titleStyle}>{title}</Text>
+          </CardItem>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -20,3 +26,8 @@ const styles = {
     paddingLeft: 15
   }
 };
+
+export default connect(
+  null,
+  actions
+)(ListItem);
