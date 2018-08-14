d5b91d652a6609d2338066a3df52e6db9035cd26
Daniel <d.chen323@gmail.com>
2018-08-14 13:21:17 -0700

    Display description upon user touch

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -5,15 +5,24 @@ import { CardItem } from "./common";
 import * as actions from "../actions";
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+    if (library.item.id === selectedLibraryId) {
+      return <Text>{library.item.description}</Text>;
+    }
+  }
+
   render() {
     const { title, id } = this.props.library.item;
     const { titleStyle } = styles;
+
     return (
       <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
         <View>
           <CardItem>
             <Text style={titleStyle}>{title}</Text>
           </CardItem>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -27,7 +36,11 @@ const styles = {
   }
 };
 
+const mapStateToProps = state => ({
+  selectedLibraryId: state.selectedLibraryId
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(ListItem);
